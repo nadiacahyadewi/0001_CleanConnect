bba2391a71155039e9344cd1dc16e0b7f246d340
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2025-06-17 23:42:59 +0700

    membuat service_http_client.dart

---
+++ lib/services/service_http_client.dart
@@ -0,0 +1,74 @@
+import 'dart:convert';
+
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:http/http.dart' as http;
+
+class ServiceHttpClient {
+  final String baseUrl = 'http://10.0.2.2:8000/api/';
+  final secureStorage = FlutterSecureStorage();
+
+  //post
+  Future<http.Response> post(String endPoint, Map<String, dynamic> body) async {
+    final url = Uri.parse("$baseUrl$endPoint");
+    try {
+      final response = await http.post(
+        url,
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+        },
+        body: jsonEncode(body),
+      );
+      return response;
+    } catch (e) {
+      throw Exception('POST request failed: $e');
+    }
+  }
+
+  //post with token
+  Future<http.Response> postWithToken(
+    String endPoint,
+    Map<String, dynamic> body,
+  ) async {
+    final token = await secureStorage.read(key: 'token');
+    final url = Uri.parse("$baseUrl$endPoint");
+    try {
+      final response = await http.post(
+        url,
+        headers: {
+          'Authorization': 'Bearer $token',
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+        },
+        body: jsonEncode(body),
+      );
+      return response;
+    } catch (e) {
+      throw Exception('POST request failed: $e');
+    }
+  }
+
+  //get
+  Future<http.Response> get(String endPoint) async {
+    final token = await secureStorage.read(key: 'token');
+    final url = Uri.parse("$baseUrl$endPoint");
+    try {
+      final response = await http.get(
+        url,
+        headers: {
+          'Authorization': 'Bearer $token',
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+        },
+      );
+      return response;
+    } catch (e) {
+      throw Exception('GET request failed: $e');
+    }
+  }
+
+  //put
+  
+
+  //delete
+}
